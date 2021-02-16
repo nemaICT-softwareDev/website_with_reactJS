9c96750595ae346cfa45d23ce34a3732372a03b2
NemaICT <neminha13@gmail.com>
2021-02-16 10:34:57 +0100

    commiting whole project with bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Basket from "./components/Basket";
+import Footer from './components/Footer';
+import store from "./store";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+import FooterPage from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+    componentWillMount() {
+        /**
+         * Here we fetch the json API server to get the list of products
+         * Check if is there any file in the localStore from previous session to be loaded in the choppingCart
+         */
+        if(localStorage.getItem("cartItems")){
+            this.setState({ cartItems: JSON.parse(localStorage.getItem("cartItems"))});
+        }
+  }
+    render(){
+    return (
+        /**
+         * The <Provider /> makes the Redux store available to any nested components that have
+         * been wrapped in the connect() function.
+         Since any React component in a React Redux app can be connected, most applications
+         will render a <Provider> at the top level, with the entire app’s component tree inside of it.
+         Normally, you can’t use a connected component unless it is nested inside of a <Provider>.
+         */
+        <Provider store={store}>
+            <div className="container">
+                <Header />
+              <hr/>
+              <div className="row">
+                <div className="col-md-8">
+                    <Filter />
+                     <hr/>
+                  <Products />
+                </div>
+                <div className="col-md-4">
+                    <Basket />
+                </div>
+              </div>
+                <FooterPage />
+            </div>
+
+        </Provider>
+    );
+  }
 }
 
 export default App;
