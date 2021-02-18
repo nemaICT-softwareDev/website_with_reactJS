32751537eb25b8ca5b6959c2b4dca27b123e069c
NemaICT <neminha13@gmail.com>
2021-02-18 16:57:16 +0100

    new photos added 2

---
+++ src/components/HLine.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class HLine extends Component{
+    render(){
+        return(
+            <div>
+                <hr/>
+            </div>
+    )}
+}
+export default HLine;
