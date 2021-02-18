47434a9f04ad77d2260fc319da771d0a2066e47a
NemaICT <neminha13@gmail.com>
2021-02-18 17:13:20 +0100

    Header class got the bootstrap container-fluid attribute and db.json has change some descriptions files

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import {Container, Jumbotron} from "react-bootstrap";
 class Header extends React.Component {
     render() {
         return (
-            <Jumbotron fluid className= "alert-info">
+            <Jumbotron className= "alert-info container-fluid">
                 <Container>
                     <h1 className="text-center title">Boodschap App</h1>
                     <h2 className="text-center subTitle">Individuele opdracht - Nemilson Lopes</h2>
