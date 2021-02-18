de5d77f42a1126598a03d65edf821ba468d9d72f
NemaICT <neminha13@gmail.com>
2021-02-18 18:32:01 +0100

    a new BckGrnImg map it's been created in the src folder with a backgroung image for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import {Container, Jumbotron} from "react-bootstrap";
 class Header extends React.Component {
     render() {
         return (
-            <Jumbotron className= "alert-info container-fluid">
-                <Container>
+            <Jumbotron className= "alert-info container-fluid header">
+                <Container >
                     <h1 className="text-center title">Boodschap App</h1>
-                    <h2 className="text-center subTitle">Individuele opdracht - Nemilson Lopes</h2>
+                    <h2 className="text-right subTitle">Individuele opdracht - Nemilson Lopes</h2>
                   </Container>
             </Jumbotron>
         );
