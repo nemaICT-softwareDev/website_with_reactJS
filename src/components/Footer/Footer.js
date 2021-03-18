05ed78dfabbe0c171468cb77327a46c00a8505f6
NemaICT <neminha13@gmail.com>
2021-03-18 20:47:44 +0100

    Footer.js is updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,44 +5,43 @@ import "./Footer.css"
 class Footer extends React.Component {
     render() {
         return (
-                <div className={"container"}>
-                    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-                        <MDBContainer fluid className="text-center text-md-left">
-                            <MDBRow>
-                                <MDBCol md="6">
-                                    <h5 className="title"> Time for fun</h5>
-                                    <blockquote className={"quote"}>
-                                        <cite>Educational games are games explicitly designed with educational purposes,
-                                        or which have incidental or secondary educational value. All types of games
-                                        may be used in an educational environment, however educational games are
-                                        games that are designed to help people learn about certain subjects,
-                                        expand concepts, reinforce development, understand a historical event or culture,
-                                            or assist them in learning a skill as they play.</cite>
-                                    </blockquote>
-                                </MDBCol>
-                                <MDBCol md="6">
-                                    <h5 className="title">Links</h5>
-                                    <ul className={"unOrderedList"}>
-                                        <li className="list-unstyled">
-                                            <a href="https://www.education.com/games/" className={"links"}>Educational Games for Kids</a>
-                                        </li>
-                                        <li className="list-unstyled">
-                                            <a href="https://www.ducksters.com/games/" className={"links"}>Educational Games</a>
-                                        </li>
-                                        <li className="list-unstyled">
-                                            <a href="https://www.funbrain.com/" className={"links"}>FunBrain</a>
-                                        </li>
-                                    </ul>
-                                </MDBCol>
-                            </MDBRow>
-                        </MDBContainer>
-                        <div className="footer-copyright text-center py-3">
-                            <MDBContainer fluid>
-                                &copy; {new Date().getFullYear()} Copyright: <a href="#"> webmaster </a>
-                            </MDBContainer>
-                        </div>
-                    </MDBFooter>
+            <MDBFooter color="blue" className="font-small pt-4 mt-4">
+                <MDBContainer fluid className="text-center text-md-left">
+                    <MDBRow>
+                        <MDBCol md="6">
+                            <h5 className="title"> Time for fun</h5>
+                            <blockquote className={"quote"}>
+                                Educational games are games explicitly designed with educational purposes,
+                                or which have incidental or secondary educational value. All types of games
+                                may be used in an educational environment, however educational games are
+                                games that are designed to help people learn about certain subjects,
+                                expand concepts, reinforce development, understand a historical event or culture,
+                                or assist them in learning a skill as they play.
+                            </blockquote>
+                        </MDBCol>
+                        <MDBCol md="6">
+                            <h5 className="title">Links</h5>
+                            <ul className={"unOrderedList"}>
+                                <li className="list-unstyled">
+                                    <a href="https://www.education.com/games/" className={"links"}>Educational Games for Kids</a>
+                                </li>
+                                <li className="list-unstyled">
+                                    <a href="https://www.ducksters.com/games/" className={"links"}>Educational Games</a>
+                                </li>
+                                <li className="list-unstyled">
+                                    <a href="https://www.funbrain.com/" className={"links"}>FunBrain</a>
+                                </li>
+                            </ul>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+                <div className="footer-copyright text-center py-3">
+                    <MDBContainer fluid>
+                        &copy; {new Date().getFullYear()} Copyright: <a href="#" className={"webmaster"}> webmaster </a>
+                    </MDBContainer>
                 </div>
+            </MDBFooter>
+
         );
     }
 }
