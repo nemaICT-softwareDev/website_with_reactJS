63d4fc3ceb921e28579c0f93df8316a7c241173d
NemaICT <neminha13@gmail.com>
2021-03-18 20:49:34 +0100

    Game.js is updated

--- src/components/Games/Game.js
+++ src/components/Games/Game.js
@@ -8,15 +8,15 @@ class Game extends Component {
         this.props.fetchGames();
     }
     render() {
-         //Here we use the map to convert the json pics files into JSX elements
+        //Here we use the map to convert the json pics files into JSX elements
         const games = this.props.games.map( (game) => (
                 <div className="col-6 col-xm-12 col-sm-3 col-md-3" key={game.id}>
-                     <div className="thumbnail">
-                               <a href={game.url}>
-                                   <div className={"picTitle"}>{game.title}</div>
-                                   <img className={"imageStyle"}  src={`pics/${game.sku}.jpg`} alt={game.title} title={game.url}/>
-                             </a><hr/>
-                         </div>
+                    <div className="thumbnail">
+                        <a href={game.url}>
+                            <div className={"picTitle"}>{game.title}</div>
+                            <img className={"imageStyle"}  src={`pics/${game.sku}.jpg`} alt={game.title} title={game.url}/>
+                        </a><hr/>
+                    </div>
                 </div>
             )
         )
@@ -28,6 +28,6 @@ class Game extends Component {
 // start browser with default state
 const mapStateToProps = (state) => ({
     games: state.games.filteredItems,
-   });
+});
 
 export default connect(mapStateToProps, {fetchGames})(Game);
