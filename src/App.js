f5ad1ea24f2be08b3dc9b3503634314f46847cf9
NemaICT <neminha13@gmail.com>
2021-03-18 20:53:42 +0100

    App.js is updated

--- src/App.js
+++ src/App.js
@@ -17,30 +17,30 @@ class App extends Component {
         if(localStorage.getItem("cartItems")){
             this.setState({ cartItems: JSON.parse(localStorage.getItem("cartItems"))});
         }
-  }
+    }
     render(){
-    return (
-        /**
-         * The <Provider /> makes the Redux store available to any nested components that have
-         * been wrapped in the connect() function. Since any React component in a React Redux app can be connected,
-         * most applications will render a <Provider> at the top level, with the entire app’s component tree inside of it.
-         * Normally, you can’t use a connected component unless it is nested inside of a <Provider>.
-         */
-        <Provider store={store}>
-          <div className="container">
-              <Header />
-              <div>
-                   <HLine />
-                   <Filter />
-                   <HLine />
-                   <Games />
-                  <HLine />
-              </div>
-             <Footer />
-         </div>
-        </Provider>
-    );
-  }
+        return (
+            /**
+             * The <Provider /> makes the Redux store available to any nested components that have
+             * been wrapped in the connect() function. Since any React component in a React Redux app can be connected,
+             * most applications will render a <Provider> at the top level, with the entire app’s component tree inside of it.
+             * Normally, you can’t use a connected component unless it is nested inside of a <Provider>.
+             */
+            <Provider store={store}>
+                <div className="container">
+                    <Header />
+                    <div>
+                        <HLine />
+                        <Filter />
+                        <HLine />
+                        <Games />
+                        <HLine />
+                    </div>
+                    <Footer />
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
